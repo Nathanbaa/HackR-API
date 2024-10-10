d1a67805870fc384cab143b8622f69aa59abda8a
nathanbaa <contact.nathanbastard@gmail.com>
2024-10-10 21:52:59 +0200

    :sparkles: add features

--- routes/public/publicRouter.js
+++ routes/public/publicRouter.js
@@ -1,10 +1,45 @@
 import express from "express";
+import crypto from "crypto";
+import { faker } from "@faker-js/faker";
 import verifyToken from "../../middleware/verifyToken.js";
 
 const publicRouter = express.Router();
 
-publicRouter.get("/home", verifyToken, (req, res) => {
+publicRouter.get("/features", verifyToken, (req, res) => {
   res.send("This is a public route accessible to everyone.");
 });
+
+publicRouter.get(
+  "/features/generate-secured-password",
+  verifyToken,
+  (req, res) => {
+    const length = 12;
+    const password = crypto.randomBytes(length).toString("hex");
+
+    res.status(200).json({ securedPassword: password });
+  }
+);
+
+publicRouter.get(
+  "/features/generate-fictive-identity",
+  verifyToken,
+  (req, res) => {
+    const fakeIdentity = {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      phoneNumber: faker.phone.number(),
+      address: {
+        street: faker.address.streetAddress(),
+        city: faker.address.city(),
+        country: faker.address.country(),
+      },
+      birthdate: faker.date.birthdate({ min: 18, max: 65, mode: "age" }),
+      avatar: faker.image.avatar(),
+    };
+
+    res.status(200).json({ identity: fakeIdentity });
+  }
+);
 
 export default publicRouter;
