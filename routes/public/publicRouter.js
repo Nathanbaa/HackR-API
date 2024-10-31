9273055ab7499d93253c31a581c73f2cc09713b0
nathanbaa <contact.nathanbastard@gmail.com>
2024-10-31 14:27:18 +0100

    :sparkles: add feature email spammer

--- routes/public/publicRouter.js
+++ routes/public/publicRouter.js
@@ -3,6 +3,7 @@ import crypto from "crypto";
 import axios from "axios";
 import fs from "fs";
 import path from "path";
+import nodemailer from "nodemailer";
 import { faker } from "@faker-js/faker";
 import verifyToken from "../../middleware/verifyToken.js";
 import dotenv from "dotenv";
@@ -305,6 +306,83 @@ publicRouter.post("/features/domain-info", verifyToken, async (req, res) => {
       message: "Error fetching domain information",
       error: error.message,
     });
+  }
+});
+
+/**
+ * @swagger
+ * /public/features/email-spammer:
+ *   post:
+ *     summary: Email spammer
+ *     description: Sends a certain number of emails to a given address with the provided content.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: The email address to spam
+ *               content:
+ *                 type: string
+ *                 description: The content of the email
+ *               nbSend:
+ *                 type: integer
+ *                 description: The number of times to send the email
+ *     responses:
+ *       200:
+ *         description: Emails successfully sent
+ *       400:
+ *         description: Missing parameters
+ *       500:
+ *         description: Error sending emails
+ */
+publicRouter.post("/features/email-spammer", verifyToken, async (req, res) => {
+  const { email, content, nbSend = 1 } = req.body;
+
+  if (!email || !content || !nbSend) {
+    return res
+      .status(400)
+      .json({ message: "Email, content, and nbSend are required" });
+  }
+
+  if (nbSend > 10) {
+    return res
+      .status(400)
+      .json({ message: "Maximum 10 emails allowed to be sent at once." });
+  }
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.GMAIL_USER,
+    to: email,
+    subject: "Not a spammm",
+    text: content,
+  };
+
+  try {
+    for (let i = 0; i < nbSend; i++) {
+      await transporter.sendMail(mailOptions);
+
+      console.log(`Email ${i + 1} sent to ${email}`);
+    }
+
+    res
+      .status(200)
+      .json({ message: `${nbSend} emails successfully sent to ${email}` });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error sending emails", error: error.message });
   }
 });
 
