304ad559e82babf7a66e66844df38416acf9a136
nathanbaa <contact.nathanbastard@gmail.com>
2024-11-20 16:01:14 +0100

    :sparkles: add feature random picture

--- routes/public/publicRouter.js
+++ routes/public/publicRouter.js
@@ -122,6 +122,31 @@ publicRouter.get(
 
 /**
  * @swagger
+ * /public/features/random-picture:
+ *   get:
+ *     summary: Get a random AI-generated picture URL
+ *     description: Fetches a random image URL from the "thispersondoesnotexist.com" API.
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 imageUrl:
+ *                   type: string
+ *                   description: URL of the generated picture.
+ *                   example: https://thispersondoesnotexist.com/image
+ */
+publicRouter.get("/features/random-picture", (req, res) => {
+  const imageUrl = "https://thispersondoesnotexist.com/image";
+
+  res.status(200).json({ imageUrl });
+});
+
+/**
+ * @swagger
  * /public/features/verify-email:
  *   post:
  *     summary: Verify email address
